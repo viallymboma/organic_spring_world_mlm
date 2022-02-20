a5201605bd9181049d2e68c3db2e8f0a9dc8cdd6
vially <viallymboma@gmail.com>
2022-02-20 14:32:45 +0000

    enabling django access to the static files

--- organic_spring_world_mlm/settings.py
+++ organic_spring_world_mlm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-tjoqa9g2p(um&kqddwe_v#(v=h0fqn!rj=6ewhjaq93+m*k66y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vial.pythonanywhere.com']
+ALLOWED_HOSTS = ['vial.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'organic_spring_world_mlm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +118,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'frontend/build/static/'
+# MEDIA_URL = 'frontend/src/assets/img/'
+# MEDIA_URL = 'https://imgbox.com/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/build/static/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/src/assets/img/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'https://imgbox.com/')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+    # os.path.join(BASE_DIR, 'reboot_frontend/build/static')
+]
+
+
+
+# STATIC_URL = 'static/'
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- organic_spring_world_mlm/urls.py
+++ organic_spring_world_mlm/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index
 
 urlpatterns = [
+    # path('', include('rebootapp.urls')),
+    path('', index),
     path('admin/', admin.site.urls),
 ]
